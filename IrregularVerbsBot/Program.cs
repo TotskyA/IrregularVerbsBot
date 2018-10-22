e4bc70f601245ef7e0ef408aa80d300713f88c21
akorablev <akorablev@yandex-team.ru>
2018-10-22 11:52:27 +0500

    minimal bot

--- IrregularVerbsBot/Program.cs
+++ IrregularVerbsBot/Program.cs
@@ -1,25 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
 
 namespace IrregularVerbsBot
 {
     public class Program
     {
+        static ITelegramBotClient botClient;
+
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter bot's token.");
+                return;
+            }
+            botClient = new TelegramBotClient(args[0]);
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine(
+              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+            );
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+            Thread.Sleep(int.MaxValue);
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+                await botClient.SendTextMessageAsync(
+                  chatId: e.Message.Chat,
+                  text: "You said:\n" + e.Message.Text
+                );
+            }
+        }
     }
 }
